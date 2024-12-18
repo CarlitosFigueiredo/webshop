76029d43a549db25ef106237c1dbc1f0b0e0d1a8
Carlos Yuri Figueiredo <92981178+CarlitosFigueiredo@users.noreply.github.com>
2024-12-18 10:55:24 -0300

    Finally episode - 4

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    /**
+     * Get the variants for products.
+     */
+    public function variants(): HasMany
+    {
+        return $this->hasMany(ProductVariant::class);
+    }
+
+    /**
+     * Get the images for products.
+     */
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
+    }
 }
--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -17,7 +17,20 @@ class ImageFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'path' => $this->faker->unique()->randomElement([
+                'media/exemple1.png',
+                'media/exemple2.png',
+                'media/exemple3.png',
+                'media/exemple4.png',
+                'media/exemple5.png',
+                'media/exemple6.png',
+                'media/exemple7.png',
+                'media/exemple8.png',
+                'media/exemple9.png',
+                'media/exemple10.png',
+                'media/exemple11.png',
+                'media/exemple12.png',
+            ])
         ];
     }
 }
